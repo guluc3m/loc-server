cd393f4a8fb2e6dbcee87a4626c1252d966eec57
Rafael Medina <rafamedgar@gmail.com>
2016-08-30 12:00:45 +0200

    Reorganized code, added /profile and /profile/edit endpoints

--- loc/util.py
+++ loc/util.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import abort, current_app, request
-from functools import wraps
-from werkzeug.exceptions import BadRequest
-from loc.models import Role, User, UserRole
+"""Helper functions."""
+
+from flask import current_app
+from loc.models import User
 
 import datetime
 import jwt
@@ -41,96 +41,25 @@ def hash_password(password):
     #TODO
     return password
 
-def login_required(f):
-    """Require a JWT token to access the decorated view.
-
-    In case the token received is not valid, the request is aborted with a
-    401 HTTP status code.
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        try:
-            jwt_token = request.get_json().get('token')
-
-        except BadRequest as e:
-            return e
-
-        if not jwt_token:
-            abort(401)
-
-        # Decode
-        try:
-            decoded = jwt.decode(jwt_token, current_app.config['SECRET_KEY'])
-
-        except jwt.ExpiredSignatureError:
-            #TODO log
-            abort(401)
-
-        # Get user
-        user = User.query.filter_by(
-            id=decoded.get('sub', -1),
-            is_deleted=False
-        ).first()
-
-        if not user:
-            abort(401)
-
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-def role_required(role):
-    """Require a JWT token and a specific user role to access the decorated view.
-
-    In case the token received is not valid, or hte user does not have the
-    required role, the request is aborted with a 401 HTTP status code.
+def user_from_jwt(token):
+    """Obtain user record from JWT token.
 
     Args:
-        role (str): Name of the role required
+        token (str): Encoded JWT token.
+
+    Returns:
+        `User` record if it exists, otherwise `None`.
     """
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            try:
-                jwt_token = request.get_json().get('token')
+    # Decode
+    try:
+        decoded = jwt.decode(token, current_app.config['SECRET_KEY'])
 
-            except BadRequest as e:
-                return e
+    except jwt.ExpiredSignatureError:
+        #TODO log
+        return None
 
-            if not jwt_token:
-                abort(401)
-
-            # Decode
-            try:
-                decoded = jwt.decode(jwt_token, current_app.config['SECRET_KEY'])
-
-            except jwt.ExpiredSignatureError:
-                #TODO log
-                abort(401)
-
-            # Get user
-            user = User.query.filter_by(
-                id=decoded.get('sub', -1),
-                is_deleted=False
-            ).first()
-
-            if not user:
-                abort(401)
-
-            # Check role
-            user_role = (
-                UserRole
-                .query
-                .join(Role, UserRole.role_id==Role.id)
-                .filter(UserRole.user_id==user.id)
-                .filter(Role.name==role)
-            ).first()
-
-            if not user_role:
-                abort(401)
-
-            return f(*args, **kwargs)
-
-        return decorated_function
-
-    return decorator
+    # Get user
+    return User.query.filter_by(
+        id=decoded.get('sub', -1),
+        is_deleted=False
+    ).first()
--- loc/views/profile.py
+++ loc/views/profile.py
@@ -2,3 +2,82 @@
 
 """Management of user profile."""
 
+from flask import Blueprint, abort, jsonify, request
+from loc.deco import login_required
+from loc.models import User
+from loc import db, util
+
+
+bp_profile = Blueprint('profile', __name__)
+
+
+@bp_profile.route('/profile')
+@login_required
+def show_profile():
+    """Show currently logged in user's profile."""
+    jwt_token = request.get_json().get('token')
+    user = util.user_from_jwt(jwt_token)
+
+    if not user:
+        abort(404)
+
+    response = {
+        'username': user.username,
+        'name': user.name,
+        'score': user.score
+    }
+
+    return jsonify(**response)
+
+@bp_profile.route('/profile/edit', methods=['GET','POST'])
+@login_required
+def update_profile():
+    """Update user profile.
+
+    When a GET request is received, returns current fields (and their values)
+    that can be edited.
+
+    After update (POST request), returns updated information.
+    """
+    received = request.get_json()
+    user = util.user_from_jwt(received.get('token'))
+
+    if not user:
+        abort(404)
+
+    if request.method == 'GET':
+        # Get information that can be edited
+        response = {
+            'name': user.name,
+            'email': user.email
+        }
+
+        return jsonify(**response)
+
+    # Update information
+    name = received.get('name', '')
+    email = received.get('email')
+
+
+    user.name = name
+    user.email = email
+
+    try:
+        correct = True
+        db.session.commit()
+
+    except Exception as e:
+        # TODO log
+        correct = False
+
+    finally:
+        if not correct:
+            db.session.rollback()
+            abort(500)
+
+    response = {
+        'name': user.name,
+        'email': user.email
+    }
+
+    return jsonify(**response)
